a7a16bc448b46fec1d6b5c0750d7a5e8d41a60e7
You-saku <53119949+You-saku@users.noreply.github.com>
2023-05-04 21:16:49 +0900

    update: mysqlの接続方法 (#2)
    * update: mysqlの接続方法
    * fix: コメント

--- go-sample-app/main.go
+++ go-sample-app/main.go
@@ -9,14 +9,30 @@ import (
 
 	"database/sql"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 
 	"go-sample-app/models"
 )
 
 func connectDB() {
-	// db
-	db, err := sql.Open("mysql", "user:secret@tcp(db:3306)/develop") // ひとまずベタ書き
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		// エラーハンドリング
+	}
+	c := mysql.Config{
+		DBName:               "develop",
+		User:                 "user",
+		Passwd:               "secret",
+		Addr:                 "db:3306",
+		Net:                  "tcp",
+		ParseTime:            true,
+		Collation:            "utf8mb4_unicode_ci",
+		Loc:                  jst,
+		AllowNativePasswords: true,
+	}
+	db, err := sql.Open("mysql", c.FormatDSN())
+	//db, err := sql.Open("mysql", "user:secret@tcp(db:3306)/develop") // 旧書き方
+
 	if err != nil {
 		panic(err)
 	}
