3e2175dfecaeb5ac06187bf297696f728a332a06
You-saku <yousaku.dev19@gmail.com>
2023-05-05 14:59:48 +0900

    fix: 不要なコメントアウトを削除

--- go-sample-app/pkg/presentation/controllers/UserController.go
+++ go-sample-app/pkg/presentation/controllers/UserController.go
@@ -11,14 +11,12 @@ import (
 
 // ひとまずCRUD作る
 func ShowUserHandler(w http.ResponseWriter, r *http.Request) {
-	// var user models.User
 	userId := strings.TrimPrefix(r.URL.Path, "/users/")
 
 	repository := Repository.UserRepository{}
 	service := services.UserService{
 		Rep: repository,
 	}
-
 	user := service.ShowUser(userId)
 
 	fmt.Fprintf(w, "id = %d name = %s\n", user.Id, user.Name)
